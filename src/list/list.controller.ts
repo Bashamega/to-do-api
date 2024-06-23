4f13ec3e0f89306beadbeec7c319e46b60286c37
Bashamega <adambashaahmednaji@gmail.com>
2024-06-23 12:40:04 +0300

    List Routes

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('list')
-export class ListController {}
+export class ListController {
+    @Get()
+    allTasks(){
+        return {
+            id:[0,1,2,3]
+        }
+    }
+    @Get(':id')
+    specificTask(@Param('id') id:string){
+        return {
+            id: id,
+            content: "Content"
+        }
+    }
+}
