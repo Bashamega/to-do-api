7d9d69e1c431817fbc22dc293c0991d1884d7259
Bashamega <adambashaahmednaji@gmail.com>
2024-06-24 07:24:08 +0300

    Prettier format 🔥

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,66 +1,75 @@
-import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ListService } from './list.service';
 import { Task, isTask } from './entities/list.entity';
 @Controller('list')
 export class ListController {
-    constructor(private readonly listService: ListService) {}
-    @Get()
-    allTasks(){
-        return this.listService.tasks()
+  constructor(private readonly listService: ListService) {}
+  @Get()
+  allTasks() {
+    return this.listService.tasks();
+  }
+  @Get(':id')
+  specificTask(@Param('id') id: number) {
+    if (this.listService.findTask(id)) {
+      return {
+        id: id,
+        message: 'Found',
+        content: this.listService.findTask(id),
+      };
+    } else {
+      throw new BadRequestException({
+        message: 'Please enter a valid id.',
+      });
     }
-    @Get(':id')
-    specificTask(@Param('id') id:number){
-        if(this.listService.findTask(id)){
-            return {
-                id: id,
-                message: "Found",
-                content: this.listService.findTask(id)
-            }
-        }else{
-            throw new BadRequestException ({
-                message: "Please enter a valid id."
-           }) 
-        }
-    }
-    @Post()
-    addNewTask(@Body() body:Task){
-        if(isTask(body)){
-            if(!this.listService.findTask(body.id)){
-                this.listService.createTask(body);
-                return {
-                    message: "Data recieved",
-                    content: body
-                }
-            }else{
-               throw new BadRequestException ({
-                    message: "Please enter a valid id. This id already exists."
-               }) 
-            }
-        }else{
-            throw new BadRequestException( {
-                message: "Please enter a valid body",
-                expectedBody:{
-                    id: 'number',
-                    title: 'string',
-                    description: 'string',
-                    done: 'boolean',
-                }
-            })
-        }
-    }
-    @Patch(':id')
-    updateTask(@Param('id') id: string, @Body() body){
+  }
+  @Post()
+  addNewTask(@Body() body: Task) {
+    if (isTask(body)) {
+      if (!this.listService.findTask(body.id)) {
+        this.listService.createTask(body);
         return {
-            message: "Success",
-            id: id,
-            body:body
-        }
+          message: 'Data recieved',
+          content: body,
+        };
+      } else {
+        throw new BadRequestException({
+          message: 'Please enter a valid id. This id already exists.',
+        });
+      }
+    } else {
+      throw new BadRequestException({
+        message: 'Please enter a valid body',
+        expectedBody: {
+          id: 'number',
+          title: 'string',
+          description: 'string',
+          done: 'boolean',
+        },
+      });
     }
-    @Delete(':id')
-    removeTask(@Param('id') id: string){
-        return {
-            message: "Success",
-            id: id,
-        }
-    }
+  }
+  @Patch(':id')
+  updateTask(@Param('id') id: string, @Body() body) {
+    return {
+      message: 'Success',
+      id: id,
+      body: body,
+    };
+  }
+  @Delete(':id')
+  removeTask(@Param('id') id: string) {
+    return {
+      message: 'Success',
+      id: id,
+    };
+  }
 }
--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -3,32 +3,31 @@ import { Task } from './entities/list.entity';
 
 @Injectable()
 export class ListService {
-    private list:Task[]  = [];
-    tasks(){
-        const keys = []
-        this.list.forEach(task => {
-            keys.push(task.id)
-        });
-        return keys
+  private list: Task[] = [];
+  tasks() {
+    const keys = [];
+    this.list.forEach((task) => {
+      keys.push(task.id);
+    });
+    return keys;
+  }
+  findTask(id: number) {
+    return this.list.find((item) => item.id === +id);
+  }
+  createTask(task: Task) {
+    this.list.push(task);
+  }
+  update(id: number, updatedData: Task) {
+    const currentData = this.findTask(id);
+    if (currentData) {
+      this.removeTask(id);
+      this.createTask(updatedData);
     }
-    findTask(id:number){
-        return this.list.find(item=>item.id === +id);
+  }
+  removeTask(id: number) {
+    const index = this.list.findIndex((item) => item.id === +id);
+    if (index >= 0) {
+      this.list.splice(index, 1);
     }
-    createTask(task:Task){
-        this.list.push(task)
-    }
-    update(id:number, updatedData:Task){
-        const currentData = this.findTask(id);
-        if(currentData){
-            this.removeTask(id);
-            this.createTask(updatedData)
-        }
-    }
-    removeTask(id:number){
-        const index = this.list.findIndex(item=>item.id === +id);
-        if(index >=0){
-            this.list.splice(index, 1)
-        }
-    }
-
+  }
 }
