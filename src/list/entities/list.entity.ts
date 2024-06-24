b49767d7180da7986637a45cb56be0606160a976
Bashamega <adambashaahmednaji@gmail.com>
2024-06-24 07:18:19 +0300

    List entity 🚀

---
+++ src/list/entities/list.entity.ts
@@ -0,0 +1,22 @@
+export class Task {
+    id: number;
+    title: string;
+    description: string;
+    done: boolean;
+    
+    constructor(id: number, title: string, description: string, done: boolean) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.done = done;
+    }
+}
+export function isTask(obj: any): obj is Task {
+    return (
+        obj &&
+        typeof obj.id === 'number' &&
+        typeof obj.title === 'string' &&
+        typeof obj.description === 'string' &&
+        typeof obj.done === 'boolean'
+    );
+}
