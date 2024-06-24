73c8523b4936a4cf174cf756a8e5c92e66e25a11
Bashamega <adambashaahmednaji@gmail.com>
2024-06-24 07:20:32 +0300

    Integrated these services: allTasks, specificTask, and addNewTask

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,26 +1,51 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-
+import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ListService } from './list.service';
+import { Task, isTask } from './entities/list.entity';
 @Controller('list')
 export class ListController {
+    constructor(private readonly listService: ListService) {}
     @Get()
     allTasks(){
-        return {
-            id:[0,1,2,3]
-        }
+        return this.listService.tasks()
     }
     @Get(':id')
-    specificTask(@Param('id') id:string){
-        return {
-            id: id,
-            message: "Found",
-            content: "Content"
+    specificTask(@Param('id') id:number){
+        if(this.listService.findTask(id)){
+            return {
+                id: id,
+                message: "Found",
+                content: this.listService.findTask(id)
+            }
+        }else{
+            throw new BadRequestException ({
+                message: "Please enter a valid id."
+           }) 
         }
     }
     @Post()
-    addNewTask(@Body() body){
-        return {
-            message: "Data recieved",
-            content: body
+    addNewTask(@Body() body:Task){
+        if(isTask(body)){
+            if(!this.listService.findTask(body.id)){
+                this.listService.createTask(body);
+                return {
+                    message: "Data recieved",
+                    content: body
+                }
+            }else{
+               throw new BadRequestException ({
+                    message: "Please enter a valid id. This id already exists."
+               }) 
+            }
+        }else{
+            throw new BadRequestException( {
+                message: "Please enter a valid body",
+                expectedBody:{
+                    id: 'number',
+                    title: 'string',
+                    description: 'string',
+                    done: 'boolean',
+                }
+            })
         }
     }
     @Patch(':id')
