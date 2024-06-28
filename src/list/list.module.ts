1da7426897a119b490826d3801de9e309d1b88e2
Bashamega <adambashaahmednaji@gmail.com>
2024-06-28 12:40:14 +0300

    Module Update

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ListController } from './list.controller';
 import { ListService } from './list.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/list.entity';
 
-@Module({controllers:[ListController], providers: [ListService]})
+@Module({imports:[TypeOrmModule.forFeature([Task])], controllers:[ListController], providers: [ListService]})
 export class ListModule {}
