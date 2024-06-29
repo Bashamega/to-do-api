b79d4bfd8e06a46a7d68d5dda0aaff78a9862bec
Bashamega <adambashaahmednaji@gmail.com>
2024-06-29 11:08:50 +0300

    Testings

--- src/list/list.controller.spec.ts
+++ src/list/list.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ListController } from './list.controller';
+import { ListService } from './list.service';
 
 describe('ListController', () => {
   let controller: ListController;
+  let service: ListService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ListController],
+      providers: [
+        {
+          provide: ListService,
+          useValue: {
+            getTasks: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ListController>(ListController);
+    service = module.get<ListService>(ListService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return an empty array of tasks', async () => {
+    expect(await controller.allTasks()).toEqual([]);
   });
 });
--- src/list/list.service.spec.ts
+++ src/list/list.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ListService } from './list.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Task } from './entities/list.entity';
 
 describe('ListService', () => {
   let service: ListService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ListService],
+      providers: [
+        ListService,
+        { provide: getRepositoryToken(Task), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<ListService>(ListService);
